954532f980219827981270477d9a8aaa33f68959
IENGROUND <my@ien.zone>
2026-05-07 23:39:55 +0900

    ⚡ perf(core): 빌드 스크립트 개선 및 의존성 업데이트
    - build.gradle.kts에 vanniktech maven publish 플러그인 적용 및 설정 추가
    - libs.versions.toml 파일에서 의존성 버전一 및 최신화
    - publish 관련 스크립트 및 Configuration 파일 삭제로 빌드 프로세스 간소화
    - Android SDK 버전 및 Kotlin 버전 업데이트로 호환성 향상

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,51 @@ plugins {
   alias(libs.plugins.android.lint) apply false
   alias(libs.plugins.compose.compiler) apply false
 }
+val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
+
+subprojects {
+  plugins.withId("com.vanniktech.maven.publish") {
+    configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+      publishToMavenCentral(automaticRelease = true)
+
+      // Artifact ID만 각 프로젝트의 이름으로 자동 설정
+      group = "zone.ien.taptargetcmp"
+      version = libs.versions.lib.version.name.get()
+      println("${group} ${project.name} ${version}")
+
+      coordinates(group.toString(), project.name, version.toString())
+
+      pom {
+        name = project.name
+        description = "A Compose Multiplatform implementation of Material Design tap targets, for feature discovery."
+        inceptionYear = "2026"
+        url = "https://github.com/ienground/tap-target-cmp"
+        licenses {
+          license {
+            name = "Apache-2.0"
+            url = "https://www.apache.org/licenses/LICENSE-2.0"
+          }
+        }
+        developers {
+          developer {
+            id = "ienground"
+            name = "Ericano Rhee"
+            url = "my@ien.zone"
+          }
+        }
+        scm {
+          url = "https://github.com/ienground/tap-target-cmp.git"
+          connection = "scm:git:https://github.com/ienground/tap-target-cmp.git"
+          developerConnection = "scm:git:https://github.com/ienground/tap-target-cmp.git"
+        }
+      }
+
+      val isPublishingToMavenLocal = gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal", ignoreCase = true) }
+      val isSnapshot = version.toString().endsWith("SNAPSHOT")
+
+      if (!isSnapshot && !isPublishingToMavenLocal) {
+        signAllPublications()
+      }
+    }
+  }
+}
