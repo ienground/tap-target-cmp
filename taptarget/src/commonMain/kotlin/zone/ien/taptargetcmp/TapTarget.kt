e34415c6ad96f0106b076e3b63462ba6b9996775
IENGROUND <my@ien.zone>
2026-06-09 17:01:56 +0900

    ♻️ refactor(core): TapTarget 컴포넌트 구조 리팩토링
    - TapTargetCoordinator 함수로 로직 분리 및 상태 관리 개선
    - Modifier 확장 함수를 통해 TapTarget 정의 방식 변경
    - TapTargetScope 및 TapTargetDefinition 클래스 추가로 컴포지션 로컬 활용 개선

--- taptarget/src/commonMain/kotlin/zone/ien/taptargetcmp/TapTarget.kt
+++ taptarget/src/commonMain/kotlin/zone/ien/taptargetcmp/TapTarget.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
